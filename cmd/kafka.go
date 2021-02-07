b9813bd70aecd2f12e2b0eb112693a44428e778d
MonielleB <monielleberger@gmail.com>
2021-02-07 15:49:22 -0300

    aula 3

--- cmd/kafka.go
+++ cmd/kafka.go
@@ -16,36 +16,33 @@ limitations under the License.
 package cmd
 
 import (
-	"github.com/MonielleB/imersao/codepix-go/application/grpc"
-	"github.com/MonielleB/imersao/codepix-go/infraestructure/db"
-	"os"
+	"fmt"
+	"github.com/MonielleB/imersao/codepix-go/application/kafka"
 
 	"github.com/spf13/cobra"
 )
 
-var portNumber int
-
-// grpcCmd represents the grpc command
-var grpcCmd = &cobra.Command{
-	Use:   "grpc",
-	Short: "Start gRPC Server",
+// kafkaCmd represents the kafka command
+var kafkaCmd = &cobra.Command{
+	Use:   "kafka",
+	Short: "Start consuming transactions using Apache Kafka",
 	Run: func(cmd *cobra.Command, args []string) {
-		database := db.ConnectDB(os.Getenv("env"))
-		grpc.StartGrpcServer(database, portNumber)
+		fmt.Println("Produzindo mensagem")
+		producer:= kafka.NewKafkaProducer()
+		kafka.Publish("Ola Kafka", "teste", producer)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(grpcCmd)
-	grpcCmd.Flags().IntVarP(&portNumber, "port", "p", 50051, "gRPC Server Port")
+	rootCmd.AddCommand(kafkaCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// grpcCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// kafkaCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// grpcCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// kafkaCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,12 @@
 /*
 Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,9 +17,8 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-
 	"github.com/spf13/cobra"
+	"os"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
@@ -28,6 +30,7 @@ var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:   "codepix",
 	Short: "Use codepix software to intermediate bank transaction with apache kafka and grpc",
+
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
